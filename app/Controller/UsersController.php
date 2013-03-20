e69baea1124a1e8715c56a424f462e579190806d
MeisterDachs <jan46@onlinehome.de>
2013-03-20 16:24:48 +0100

    Benutzerprofil

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -66,20 +66,20 @@ $this->Session->setFlash('Sie wurden ausgeloggt');
                 $this->Session->setFlash(__('The user could not be saved. Please, try again.'));
             }
         }
-   
-			
-
+  
 		}
 
     public function edit($id = null) {
         $this->User->id = $id;
+				
         if (!$this->User->exists()) {
             throw new NotFoundException(__('Invalid user'));
         }
+
         if ($this->request->is('post') || $this->request->is('put')) {
             if ($this->User->save($this->request->data)) {
                 $this->Session->setFlash(__('The user has been saved'));
-                $this->redirect(array('action' => 'index'));
+                $this->redirect(array('controller' => 'entries', 'action' => 'index'));
             } else {
                 $this->Session->setFlash(__('The user could not be saved. Please, try again.'));
             }
---
+++ app/View/Users/edit.ctp
@@ -0,0 +1,13 @@
+<!-- File: /app/View/Users/edit.ctp -->
+
+<h1>Edit Profile</h1>
+<?php
+	echo "Username: ".$this->request->data['User']['username']."</br>";
+	echo "Role: ".$this->request->data['User']['role']."</br></br>";
+    echo $this->Form->create('User');
+    echo $this->Form->input('semester', array('maxlength' => '2'));
+    echo $this->Form->input('matnr',array('maxlength' => '6'));
+	echo $this->Form->input('course');
+    echo $this->Form->end('Save User');
+	
+	?>
