968cbf994afbc29fee5aa40316bd2fd5dbdc2bba
MeisterDachs <jan46@onlinehome.de>
2013-04-05 14:27:37 +0200

    Comments Model geht

--- app/Model/Comment.php
+++ app/Model/Comment.php
@@ -3,9 +3,11 @@
 class Comment extends AppModel {
 
 public $belongsTo = array('User','Entry');
-public $validate = array('text' => array('required' => true));
+public $validate = array('text' => array(
+										'rule' => 'notEmpty', // or: array('ruleName', 'param1', 'param2' ...)
+										'required' => true
 
-
+										));
 }
 
 ?>
