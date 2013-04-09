7913b95169ba736dd88a3a85edac9056745337c8
MeisterDachs <jan46@onlinehome.de>
2013-04-09 17:05:19 +0200

    default.png

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -56,7 +56,7 @@ $this->Session->setFlash('Sie wurden ausgeloggt');
 		 $this->Session->setFlash(__('User bereits vorhanden'));
 		 else {
 				$this->request->data['User']['role'] = 'author';
-				$this->request->data['User']['pic'] = 'default.jpg';
+				$this->request->data['User']['pic'] = 'default.png';
 				$this->User->create();
 				if ($this->User->save($this->request->data)) {
 					$this->Session->setFlash(__('The user has been saved'));
